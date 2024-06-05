be18f4a3740effc16ed05a697165f2a135a2c01c
amin Mardani <54062934+aminmardani@users.noreply.github.com>
2024-06-05 17:05:41 +0330

    Create bankfile.sql

---
+++ bankfile.sql
@@ -0,0 +1,5 @@
+select t.*, t.rowid from hld.payment t where t.bank_file_id=2988199;
+select t.*, t.rowid from hld.bank_file t where t.id=2942085;
+select t.*, t.rowid from hld.payment_sending_response t where t.id=17679309;
+select t.*, t.rowid from hld.payment_sending_rspn_log t where t.payment_sending_response_id=17679309;
+
